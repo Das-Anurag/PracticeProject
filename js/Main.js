1e79eb50cc1dd53203a370a5141fada99ec54b8a
Das-Anurag <rmicrosoft365@gmail.com>
2026-08-03 07:50:46 +0530

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -1,13 +1,1 @@
 alert("Main.js Loaded");
-import * as THREE from "https://unpkg.com/three@0.179.1/build/three.module.js";
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(400,300);
-renderer.setColor(0xff0000);
-document.body.appendChild(renderer.domElement);
