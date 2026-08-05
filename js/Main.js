78a1a4bd0beb2ec2abedd3a1a75f57fcfa304502
Das-Anurag <rmicrosoft365@gmail.com>
2026-08-05 20:31:49 +0530

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -7,7 +7,9 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(5, 5, 5);
 
 scene.add(light);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 
+scene.add(ambientLight);
 // Create the Camera
 const camera = new THREE.PerspectiveCamera(
     120,
